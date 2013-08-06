0ef29c0b442b323612ec94acb42d350f5b91f169
ruslan.palagin <ruslan.palagin@masterofcode.com>
2013-08-06 20:15:00 +0300

    add getters & setters

--- Entity/Page.php
+++ Entity/Page.php
@@ -127,4 +127,359 @@ class Page
 
     //--
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set metaTitle
+     *
+     * @param string $metaTitle
+     * @return Page
+     */
+    public function setMetaTitle($metaTitle)
+    {
+        $this->metaTitle = $metaTitle;
+    
+        return $this;
+    }
+
+    /**
+     * Get metaTitle
+     *
+     * @return string 
+     */
+    public function getMetaTitle()
+    {
+        return $this->metaTitle;
+    }
+
+    /**
+     * Set metaKeywords
+     *
+     * @param string $metaKeywords
+     * @return Page
+     */
+    public function setMetaKeywords($metaKeywords)
+    {
+        $this->metaKeywords = $metaKeywords;
+    
+        return $this;
+    }
+
+    /**
+     * Get metaKeywords
+     *
+     * @return string 
+     */
+    public function getMetaKeywords()
+    {
+        return $this->metaKeywords;
+    }
+
+    /**
+     * Set metaDescription
+     *
+     * @param string $metaDescription
+     * @return Page
+     */
+    public function setMetaDescription($metaDescription)
+    {
+        $this->metaDescription = $metaDescription;
+    
+        return $this;
+    }
+
+    /**
+     * Get metaDescription
+     *
+     * @return string 
+     */
+    public function getMetaDescription()
+    {
+        return $this->metaDescription;
+    }
+
+    /**
+     * Set metaAuthor
+     *
+     * @param string $metaAuthor
+     * @return Page
+     */
+    public function setMetaAuthor($metaAuthor)
+    {
+        $this->metaAuthor = $metaAuthor;
+    
+        return $this;
+    }
+
+    /**
+     * Get metaAuthor
+     *
+     * @return string 
+     */
+    public function getMetaAuthor()
+    {
+        return $this->metaAuthor;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Page
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set descriptionFormatter
+     *
+     * @param string $descriptionFormatter
+     * @return Page
+     */
+    public function setDescriptionFormatter($descriptionFormatter)
+    {
+        $this->descriptionFormatter = $descriptionFormatter;
+    
+        return $this;
+    }
+
+    /**
+     * Get descriptionFormatter
+     *
+     * @return string 
+     */
+    public function getDescriptionFormatter()
+    {
+        return $this->descriptionFormatter;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Page
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set bodyFormatter
+     *
+     * @param string $bodyFormatter
+     * @return Page
+     */
+    public function setBodyFormatter($bodyFormatter)
+    {
+        $this->bodyFormatter = $bodyFormatter;
+    
+        return $this;
+    }
+
+    /**
+     * Get bodyFormatter
+     *
+     * @return string 
+     */
+    public function getBodyFormatter()
+    {
+        return $this->bodyFormatter;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Page
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string 
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set isPublished
+     *
+     * @param boolean $isPublished
+     * @return Page
+     */
+    public function setIsPublished($isPublished)
+    {
+        $this->isPublished = $isPublished;
+    
+        return $this;
+    }
+
+    /**
+     * Get isPublished
+     *
+     * @return boolean 
+     */
+    public function getIsPublished()
+    {
+        return $this->isPublished;
+    }
+
+    /**
+     * Set isOnHomepage
+     *
+     * @param boolean $isOnHomepage
+     * @return Page
+     */
+    public function setIsOnHomepage($isOnHomepage)
+    {
+        $this->isOnHomepage = $isOnHomepage;
+    
+        return $this;
+    }
+
+    /**
+     * Get isOnHomepage
+     *
+     * @return boolean 
+     */
+    public function getIsOnHomepage()
+    {
+        return $this->isOnHomepage;
+    }
+
+    /**
+     * Set isPartial
+     *
+     * @param boolean $isPartial
+     * @return Page
+     */
+    public function setIsPartial($isPartial)
+    {
+        $this->isPartial = $isPartial;
+    
+        return $this;
+    }
+
+    /**
+     * Get isPartial
+     *
+     * @return boolean 
+     */
+    public function getIsPartial()
+    {
+        return $this->isPartial;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Page
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Page
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Page
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
 }
